94014f0e9a3d9e4df1339a3dc39dd83cbac9bab6
Susmita Dey <susmitadey475@gmail.com>
2022-10-13 22:09:34 +0530

    Adding filles

---
+++ index.js
@@ -0,0 +1,56 @@
+const MoodContractAddress = "0xc090d88335F5CF375740d2e3F7ea5591D9C17dbb";
+const MoodContractABI = [
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_mood",
+        type: "string",
+      },
+    ],
+    name: "setMood",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getMood",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+let MoodContract;
+let signer;
+
+const provider = new ethers.providers.Web3Provider(window.ethereum, "goerli");
+
+provider.send("eth_requestAccounts", []).then(() => {
+  provider.listAccounts().then((accounts) => {
+    signer = provider.getSigner(accounts[0]);
+    MoodContract = new ethers.Contract(
+      MoodContractAddress,
+      MoodContractABI,
+      signer
+    );
+  });
+});
+
+async function getMood() {
+  const getMoodPromise = MoodContract.getMood();
+  const Mood = await getMoodPromise;
+  console.log(Mood);
+}
+
+async function setMood() {
+  const mood = document.getElementById("mood").value;
+  const setMoodPromise = MoodContract.setMood(mood);
+  await setMoodPromise;
+}
